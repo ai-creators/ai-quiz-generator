06952c13791d5de6d5dd2eb6baa6233bdfa1d39e
Anthony Mclamb <adylanmclamb@yahoo.com>
2024-07-07 01:03:50 -0400

    Setting up create account

--- backend/src/main/java/com/aiquizgenerator/backend/account/AccountController.java
+++ backend/src/main/java/com/aiquizgenerator/backend/account/AccountController.java
@@ -1,9 +1,11 @@
 package com.aiquizgenerator.backend.account;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aiquizgenerator.backend.account.dto.request.CreateAccountDto;
 import com.aiquizgenerator.backend.account.services.AccountService;
 
 import lombok.RequiredArgsConstructor;
@@ -16,6 +18,8 @@ public class AccountController {
     private final AccountService accountService;
 
     @PostMapping
-    public String createAccount() {
+    public String createAccount(
+            @RequestBody() CreateAccountDto createAccountDto) {
+        return createAccountDto.getSub() + createAccountDto.getUsername();
     }
 }
--- backend/src/main/java/com/aiquizgenerator/backend/account/dto/request/CreateAccountDto.java
+++ backend/src/main/java/com/aiquizgenerator/backend/account/dto/request/CreateAccountDto.java
@@ -1,5 +1,21 @@
 package com.aiquizgenerator.backend.account.dto.request;
 
-public class CreateAccountDto {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
+public class CreateAccountDto {
+    @NotBlank(message = "A sub is required")
+    private String sub;
+
+    @NotBlank(message = "Username cannot be blank")
+    @Size(max = 50, message = "Username can be at most 50 characters")
+    private String username;
+
+    public String getSub() {
+        return sub;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
